b8286ab2515fbb45ddf099d88828b86d0084903f
Martin Hardselius <martin.hardselius@gmail.com>
2019-10-11 12:28:36 +0200

    Add antibody files

---
+++ antibody/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if command -v brew >/dev/null 2>&1; then
+	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
+	brew install antibody
+else
+	curl -sL https://git.io/antibody | sh -s
+fi
+
+antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+antibody update
