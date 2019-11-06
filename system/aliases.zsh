ad3a40d97fb0bf44911a9fed0d6944323438f9b8
Martin Hardselius <martin.hardselius@gmail.com>
2019-11-06 08:33:53 +0100

    Add exa as fist-choice ls replacement

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,14 +1,20 @@
 #! /usr/bin/env zsh
 
+if command -v exa &>/dev/null; then
+  alias ls="exa -F --group-directories-first"
+  alias l="exa -lah --group-directories-first"
+  alias ll="exa -l --group-directories-first"
+  alias la="exa -a --group-directories-first"
+
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
+elif command -v gls &>/dev/null; then
   alias ls="gls -F --color --group-directories-first"
   alias l="gls -lAh --color --group-directories-first"
   alias ll="gls -l --color --group-directories-first"
   alias la="gls -A --color --group-directories-first"
+
 else
   if [[ "$(uname -s)"  == "Darwin" ]]; then
     alias ls="ls -FG"
