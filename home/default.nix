8da0b7503f258789eb2033484be14ddc40b288a9
Martin Hardselius <martin@hardselius.dev>
2021-03-15 20:44:03 +0100

    Consolidate home-manager config: kitty

--- home/default.nix
+++ home/default.nix
@@ -5,7 +5,6 @@ rec {
     ./git.nix
     ./newsboat.nix
     ./shells.nix
-    ./kitty.nix
   ];
 
   home = {
@@ -72,6 +71,28 @@ rec {
       enable = true;
       enableBashIntegration = true;
       enableZshIntegration = true;
+    };
+
+    kitty = {
+      enable = true;
+
+      settings = {
+        font_family = "Hack Regular";
+        font_size = "14.0";
+        adjust_line_height = "120%";
+        disable_ligatures = "cursor"; # disable ligatures when cursor is on them
+
+        # Window layout
+        hide_window_decorations = "titlebar-only";
+        window_padding_width = "10";
+
+        # Tab bar
+        tab_bar_edge = "top";
+        tab_bar_style = "powerline";
+        tab_title_template = "Tab {index}: {title}";
+        active_tab_font_style = "bold";
+        inactive_tab_font_style = "normal";
+      };
     };
   };
 
