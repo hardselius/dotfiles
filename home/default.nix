3835b6709e4e682010469f7fccb7749c82687ecc
Martin Hardselius <martin@hardselius.dev>
2021-03-22 12:49:15 +0100

    Consolidate home-manager config: shells

--- home/default.nix
+++ home/default.nix
@@ -4,10 +4,6 @@ let
   home_directory = "${config.home.homeDirectory}";
 in
 rec {
-  imports = [
-    ./shells.nix
-  ];
-
   home = {
     stateVersion = "20.09";
 
@@ -39,6 +35,7 @@ rec {
       nodePackages.vim-language-server
       pass
       plantuml
+      pure-prompt
       pywal
       renameutils
       ripgrep
@@ -107,6 +104,88 @@ rec {
         active_tab_font_style = "bold";
         inactive_tab_font_style = "normal";
       };
+    };
+
+    zsh = {
+      enable = true;
+
+      cdpath = [
+        "."
+        "~"
+      ];
+
+      defaultKeymap = "viins";
+      dotDir = ".config/zsh";
+
+      history = {
+        size = 50000;
+        save = 500000;
+        ignoreDups = true;
+        share = true;
+      };
+
+      sessionVariables = {
+        CLICOLOR = true;
+        GPG_TTY = "$TTY";
+        PATH = "$PATH:$HOME/.local/bin";
+      };
+
+      shellAliases = {
+        tf = "terraform";
+        restartaudio = "sudo killall coreaudiod";
+      };
+
+      profileExtra = ''
+        export GPG_TTY=$(tty)
+
+        if ! pgrep -x "gpg-agent" > /dev/null; then
+            ${pkgs.gnupg}/bin/gpgconf --launch gpg-agent
+        fi
+      '';
+
+      initExtra = ''
+        export KEYTIMEOUT=1
+        export SSH_AUTH_SOCK=$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)
+
+        vi-search-fix() {
+          zle vi-cmd-mode
+          zle .vi-history-search-backward
+        }
+        autoload vi-search-fix
+        zle -N vi-search-fix
+        bindkey -M viins '\e/' vi-search-fix
+
+        bindkey "^?" backward-delete-char
+
+        resume() {
+          fg
+          zle push-input
+          BUFFER=""
+          zle accept-line
+        }
+        zle -N resume
+        bindkey "^Z" resume
+
+        ls() {
+            ${pkgs.coreutils}/bin/ls --color=auto --group-directories-first "$@"
+        }
+
+        autoload -U promptinit; promptinit
+
+        # Configure pure-promt
+        prompt pure
+        zstyle :prompt:pure:prompt:success color green
+
+        # The next line updates PATH for the Google Cloud SDK.
+        if [ -f '/Users/martin/bin/google-cloud-sdk/path.zsh.inc' ]; then
+          . '/Users/martin/.local/bin/google-cloud-sdk/path.zsh.inc'
+        fi
+
+        # The next line enables shell command completion for gcloud.
+        if [ -f '/Users/martin/.local/bin/google-cloud-sdk/completion.zsh.inc' ]; then
+          . '/Users/martin/.local/bin/google-cloud-sdk/completion.zsh.inc'
+        fi
+      '';
     };
 
     git = {
