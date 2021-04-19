a1694b42e5e8f10f8c5b43eb236e316209e78283
Martin Hardselius <martin@hardselius.dev>
2021-04-19 21:41:52 +0200

    Move kitty config into separate file

--- home/default.nix
+++ home/default.nix
@@ -4,6 +4,10 @@ let
   home_directory = "${config.home.homeDirectory}";
 in
 rec {
+  imports = [
+    ./kitty.nix
+  ];
+
   home = {
     stateVersion = "20.09";
 
@@ -80,28 +84,6 @@ rec {
 
     go = {
       enable = true;
-    };
-
-    kitty = {
-      enable = true;
-
-      settings = {
-        font_family = "Hack Regular";
-        font_size = "14.0";
-        adjust_line_height = "120%";
-        disable_ligatures = "cursor"; # disable ligatures when cursor is on them
-
-        # Window layout
-        hide_window_decorations = "titlebar-only";
-        window_padding_width = "10";
-
-        # Tab bar
-        tab_bar_edge = "top";
-        tab_bar_style = "powerline";
-        tab_title_template = "Tab {index}: {title}";
-        active_tab_font_style = "bold";
-        inactive_tab_font_style = "normal";
-      };
     };
 
     zsh = {
