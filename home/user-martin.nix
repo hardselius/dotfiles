58761b72222758b08bced24ac62593e176080990
Martin Hardselius <martin@hardselius.dev>
2021-05-23 19:09:08 +0200

    Add himalaya configuration

--- home/user-martin.nix
+++ home/user-martin.nix
@@ -1,8 +1,16 @@
-{ ... }: 
+{ config, pkgs, ... }:
 let
   name = "Martin Hardselius";
   email = "martin@hardselius.dev";
-in {
+  home_directory = "${config.home.homeDirectory}";
+
+  tomlFormat = pkgs.formats.toml { };
+in
+{
+  home.packages = with pkgs; [
+    himalaya
+  ];
+
   programs = {
     git = {
       userName = "${name}";
@@ -21,4 +29,30 @@ in {
       };
     };
   };
+
+  xdg.configFile."himalaya/config.toml" = {
+    source = tomlFormat.generate "himalaya-config" {
+      name = "${name}";
+      downloads-dir = "${home_directory}/Downloads";
+      signature = ''
+        --
+        ${name}
+      '';
+      fastmail = {
+        default = true;
+        email = "${email}";
+        imap-host = "imap.fastmail.com";
+        imap-port = 993;
+        imap-login = "${email}";
+        imap-passwd-cmd = "${pkgs.pass}/bin/pass show email/imap.fastmail.com";
+        smtp-host = "smtp.fastmail.com";
+        smtp-port = 587;
+        smtp-starttls = true;
+        smtp-login = "${email}";
+        smtp-passwd-cmd = "${pkgs.pass}/bin/pass show email/smtp.fastmail.com";
+      };
+    };
+  };
 }
+
+
