b1e7d5446423371c132ac10893b2b72ca10287b0
Martin Hardselius <martin@hardselius.dev>
2021-09-12 23:49:50 +0200

    Remove some unused stuff

--- 10-darwin/default.nix
+++ 10-darwin/default.nix
@@ -7,14 +7,8 @@
 
   environment = {
     systemPackages = with pkgs; [
-      alacritty
       terminal-notifier
     ];
-
-    variables = {
-      # https://github.com/nix-community/home-manager/issues/423
-      TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
-    };
   };
 
   programs.bash = {
