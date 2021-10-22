48af200e16dfc31bdff742de447a3e4f6b6f474c
Martin Hardselius <martin@hardselius.dev>
2021-10-22 22:57:55 +0200

    Clean up overlays

--- 40-overlays/pkgs.nix
+++ 40-overlays/pkgs.nix
@@ -2,4 +2,42 @@ final: prev: {
   pure-prompt = prev.pure-prompt.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [ ./pure-zsh.patch ];
   });
+
+  # steampipe = with super; buildGoModule rec {
+  #   pname = "steampipe";
+  #   version = "0.8.5";
+
+  #   src = fetchFromGitHub {
+  #     owner = "turbot";
+  #     repo = "steampipe";
+  #     rev = "v${version}";
+  #     sha256 = "sha256-3vetSUJwCeaBzKj+635siskfcDPs/kkgCH954cg/REA=";
+  #   };
+
+  #   vendorSha256 = "sha256-TGDFNHWWbEy1cD7b2yPqAN7rRrLvL0ZX/R3BWGRWjjw=";
+
+  #   doCheck = false;
+
+  #   nativeBuildInputs = [ installShellFiles ];
+
+  #   ldflags = [
+  #     "-s"
+  #     "-w"
+  #   ];
+
+  #   postInstall = ''
+  #     INSTALL_DIR=$(mktemp -d)
+  #     installShellCompletion --cmd steampipe \
+  #       --bash <($out/bin/steampipe --install-dir $INSTALL_DIR completion bash) \
+  #       --fish <($out/bin/steampipe --install-dir $INSTALL_DIR completion fish) \
+  #       --zsh <($out/bin/steampipe --install-dir $INSTALL_DIR completion zsh)
+  #   '';
+
+  #   meta = with lib; {
+  #     homepage = "https://steampipe.io/";
+  #     description = "select * from cloud;";
+  #     license = licenses.agpl3;
+  #     maintainers = with maintainers; [ hardselius ];
+  #   };
+  # };
 }
