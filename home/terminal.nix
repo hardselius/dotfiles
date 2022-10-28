5c1c83214b23f82657e1325b0c7c17588fd8d3f9
Martin Hardselius <martin@hardselius.dev>
2022-10-29 00:27:06 +0200

    Update Alacritty configuration

--- home/terminal.nix
+++ home/terminal.nix
@@ -5,11 +5,9 @@ in
 {
   programs = {
     alacritty.enable = true;
-    alacritty.settings.window = {
-      padding.x = 10;
-      padding.y = 10;
-      dynamic_title = true;
-    };
+    alacritty.settings.window.padding.x = 10;
+    alacritty.settings.window.padding.y = 10;
+    alacritty.settings.window.dynamic_title = true;
     alacritty.settings.scrolling.history = 10000;
     alacritty.settings.key_bindings = [
       {
@@ -17,16 +15,31 @@ in
         mods = "Control";
         chars = "\\x11";
       }
-    ] ++ lib.optionals pkgs.stdenv.isDarwin [
-      # macOS tabs
-      # System Preferences > General > Prefer tabs: always
-      # must be set in order for this to work.
-      {
-        key = "T";
-        mods = "Command";
-        action = "CreateNewWindow";
-      }
     ];
+    alacritty.settings.colors.primary = {
+      background = "#262626";
+      foreground = "#ffffff";
+    };
+    alacritty.settings.colors.normal = {
+      black = "0x000000";
+      red = "0xff0000";
+      green = "0x5f8700";
+      yellow = "0xffff00";
+      blue = "0x87d7ff";
+      magenta = "0xd7d787";
+      cyan = "0xffd7af";
+      white = "0x666666";
+    };
+    alacritty.settings.colors.light = {
+      black = "0x333333";
+      red = "0xffafaf";
+      green = "0x00875f";
+      yellow = "0xffd700";
+      blue = "0x5f87d7";
+      magenta = "0xafaf87";
+      cyan = "0xff8787";
+      white = "0xffffff";
+    };
     alacritty.settings.font = {
       normal.family = "${fontFamily}";
       bold.family = "${fontFamily}";
