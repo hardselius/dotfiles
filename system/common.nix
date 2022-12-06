eac2a2ab71ccb1058a818ea6d10b3133d7d594a3
Martin Hardselius <martin@hardselius.dev>
2022-12-06 10:13:52 +0100

    Remove iohk cache

--- system/common.nix
+++ system/common.nix
@@ -16,14 +16,10 @@
   nix.settings.substituters = [
     "https://cache.nixos.org/"
     "https://hardselius.cachix.org"
-    "https://hydra.iohk.io"
-    "https://iohk.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "hardselius.cachix.org-1:PoN90aQw2eVMwfAy0MS6V9T2exWlgtHOUBBSnthXAl4="
-    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
   ];
 
   programs.zsh.enable = true;
