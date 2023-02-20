7325ced7c28f2bc7e797d0b0fb1f4dadedcb0823
Martin Hardselius <martin@hardselius.dev>
2023-02-20 13:16:05 +0100

    Go back to using unstable git

--- home/git.nix
+++ home/git.nix
@@ -6,7 +6,7 @@ in
 {
   programs.git = {
     enable = true;
-    package = pkgs.pkgs-stable.git;
+    package = pkgs.git;
     userEmail = user-info.email;
     userName = user-info.fullName;
     extraConfig = {
